876693722a8d7ec9c8d77b1faa381c4e536ec9f5
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-20 12:04:48 +0300

    create functions.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+  function returnFirstTwoDrivers(){
+  returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+   const returnFirstTwoDrivers// => ['Antonia', 'Nuru']
+  }  
+function returnLastTwoDrivers(){
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+   returnLastTwoDrivers=// => ['Amari', 'Mo']   
+};
+function selectingDrivers(){
+    returnselectingDrivers//=>[1]
+    //return FirstTwoDrivers =>1
+}
+  function selectingDrivers(){
+    returnselectingDrivers//=>[0]
+    //return LastTwoDrivers//=>0
+  }
+  function createFareMultiplier(){
+    "returns a function'"
+      const fareMultiplier = (2)
+      (fareMultiplier)
+    };
+    function fareDoubler() {
+        "return is a function'"
+        ();
+        };
+        return doubles 
+            fareDoubler(10 ,20)
+          ; 
